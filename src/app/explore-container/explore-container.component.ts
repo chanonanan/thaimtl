58791866fdcc43ac3cee630501993f094ddf3f7e
Chanon Ungpansattawong (Non) <chanon.ungpansattawong@thomsonreuters.com>
2022-03-12 16:58:49 +0700

    lint fix

--- src/app/explore-container/explore-container.component.ts
+++ src/app/explore-container/explore-container.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-explore-container',
-  templateUrl: './explore-container.component.html',
-  styleUrls: ['./explore-container.component.scss'],
+	selector: 'app-explore-container',
+	templateUrl: './explore-container.component.html',
+	styleUrls: ['./explore-container.component.scss'],
 })
 export class ExploreContainerComponent implements OnInit {
   @Input() name: string;
