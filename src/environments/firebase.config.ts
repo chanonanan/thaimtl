9b60fa877c202efcd557fa29faa6745b5243711e
Chanon Ungpansattawong (Non) <chanon.ungpansattawong@thomsonreuters.com>
2022-03-13 14:30:05 +0700

    Add firebase rt db

---
+++ src/environments/firebase.config.ts
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+	projectId: 'novel-db-c6b6d',
+	appId: '1:1055076608765:web:801d859ee99da413f2ac94',
+	databaseURL: 'https://novel-db-c6b6d-default-rtdb.firebaseio.com',
+	storageBucket: 'novel-db-c6b6d.appspot.com',
+	apiKey: 'AIzaSyDUMXVFbPoXUHPNzRWkWsu2eRVeosyWf5o',
+	authDomain: 'novel-db-c6b6d.firebaseapp.com',
+	messagingSenderId: '1055076608765',
+	measurementId: 'G-C7VVCT0ZQN',
+}
