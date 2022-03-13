8173aaaa93e11a120f4598d0b84eb4fe938016b0
Chanon Ungpansattawong (Non) <chanon.ungpansattawong@thomsonreuters.com>
2022-03-13 17:26:09 +0700

    Detail page with chapter list

--- src/app/pages/details/details.page.ts
+++ src/app/pages/details/details.page.ts
@@ -1,22 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnDestroy, OnInit, QueryList, ViewChildren } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { NavController } from '@ionic/angular';
+import { Observable } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
+import { NovelsService } from '../../api/novels.service';
 
 @Component({
 	selector: 'app-details',
 	templateUrl: './details.page.html',
 	styleUrls: ['./details.page.scss'],
 })
-export class DetailsPage implements OnInit {
+export class DetailsPage implements OnInit, OnDestroy {
 	novelId: string;
+	list$: Observable<{ chapter: number, lastRead: boolean }[]>;
+
+	@ViewChildren("chapterList", { read: ElementRef }) renderedchapterList: QueryList<ElementRef>;
+
+	private timeout;
 
 	constructor(
 		private route: ActivatedRoute,
 		private navCtrl: NavController,
+		private novelService: NovelsService,
 	) { }
 
 	ngOnInit() {
 		this.novelId = this.route.snapshot.queryParamMap.get('novelId');
+		this.list$ = this.novelService.getDetails(this.novelId).pipe(
+			map(details => {
+				return new Array(details.total).fill(null).map((_, index) => ({
+					chapter: index + 1,
+					lastRead: details.lastRead === index + 1
+				}));
+			}),
+			tap((list) => this.scrollIntoView(list.find(item => item.lastRead)?.chapter))
+		);
+	}
+
+	ngOnDestroy() {
+		clearTimeout(this.timeout);
 	}
 
 	public openItem(chapter: number): void {
@@ -26,6 +48,14 @@ export class DetailsPage implements OnInit {
 				chapter
 			}
 		});
+	}
+
+	public scrollIntoView(chapter: number) {
+		// Make sure that list is already rendered
+		this.timeout = setTimeout(() => {
+			const chapterToScrollOn = this.renderedchapterList.toArray();
+			chapterToScrollOn[chapter - 1].nativeElement.scrollIntoView({behavior: 'smooth'});
+		}, 1000);
 	}
 
 }
